11708f0e79b09f66b865d109e488ba4c7c16742c
Jitendra Kumar <jitendra@example.com>
2025-09-03 02:09:24 +0530

    new rg

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "sgstatefile123"
+    container_name       = "jitu"
+    key                  = "terraform.tfstate"
+  }
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.36.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "76095b6e-0585-402e-8e43-281835aebfd1"
+}
+
+resource "azurerm_resource_group" "rg" {
+    name = "rg-test"
+    location = "East US"
+}
